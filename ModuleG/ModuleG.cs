189246036e7d5ad83f1543533144c075b83ad7c0
马嘉远 <944414275@qq.com>
2019-08-16 15:41:56 +0800

    Add project files.

---
+++ ModuleG/ModuleG.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Prism.Logging;
+using Microsoft.Practices.Prism.Modularity;
+using ModuleTracking;
+
+namespace ModularityWithUnity.Desktop
+{
+    //class ModuleG:IModule
+    //{
+
+    //}
+}
